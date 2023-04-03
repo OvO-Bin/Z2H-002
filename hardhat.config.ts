657d0a74de061c77790290a7d7627340e3c66271
Bin <yangbin03@huohua.cn>
2023-04-03 18:49:32 +0800

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import '@openzeppelin/hardhat-upgrades';
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import "./tasks";
+
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
+setGlobalDispatcher(proxyAgent);
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  // Default network when you don't specify "--network {network_name}"
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      // {
+      //  mnemonic: process.env.MNEMONIC,
+      //  count: 20,
+      //}
+    },
+    bnbtest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId:97,
+      gasPrice: 20000000000,
+      accounts: [""],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: ''
+    }
+  }
+};
+
+export default config;
